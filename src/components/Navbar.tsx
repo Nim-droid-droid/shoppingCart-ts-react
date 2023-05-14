fd455906f56ca877fe83ef0f4b29e2e440c5c4d5
Nim-droid-droid <levkoz2222@outlook.com>
2023-05-14 22:57:14 +0100

    set up links to other pages inside Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
-import { Container, Navbar as NavBarBS } from "react-bootstrap"
+import { Container, Nav, Navbar as NavBarBS } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 
-export function Nav(){
+
+export function NavBAR(){
   
   return(
     <NavBarBS>
-      <Container>Navbar</Container>
+      <Container>
+        <Nav>
+          <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
+        </Nav>
+
+        cartIcon goes here
+
+      </Container>
     </NavBarBS>
     
   )
