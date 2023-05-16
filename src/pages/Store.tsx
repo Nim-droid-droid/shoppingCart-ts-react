4aadf13ce68fdc1e6270d2ee5ebf7b05a3f18d75
Nim-droid-droid <levkoz2222@outlook.com>
2023-05-16 21:15:26 +0100

    add store items to Store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,16 @@
+import { Row, Col } from "react-bootstrap"
+import storeItems from "../data/items.json"
+
 export function Store(){
   return(
     <>
       <h1>Store Page</h1>
+      <Row>
+        {storeItems.map(item => (
+          <Col>{JSON.stringify(item)}</Col>
+        ))
+        }
+      </Row>
     </>
   )
 }
