6c2a33b373db20de4f09011036a3513b132045f4
Nim-droid-droid <levkoz2222@outlook.com>
2023-05-18 22:55:50 +0100

    added description to imgs on the Store page detailing each item's price & name

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -9,7 +9,15 @@ type StoreItemProps = {
 }
 
 export function StoreItem({id, name, price, imgURL}: StoreItemProps){
-  return <Card>
+  return (
+  <Card>
     <Card.Img variant="top" src={imgURL} height="200px" style={{objectFit: "cover"}}/>
+    <Card.Body className="d-flex flex-column">
+      <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+        <span className="fs-2">{name}</span>
+        <span className="ms-2 text-muted">{price}</span>
+      </Card.Title>
+    </Card.Body>
   </Card>
+  )
 }
