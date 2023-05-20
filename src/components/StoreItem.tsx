cbeb299a579b104a74aa59244482cdb422e38770
Nim-droid-droid <levkoz2222@outlook.com>
2023-05-20 22:57:11 +0100

    add +/- btns for shopping cart on each img card for Store page

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
+import {formatCurrency} from "../utilities/formatCurrency"
 
 // interface
 type StoreItemProps = {
@@ -9,14 +10,44 @@ type StoreItemProps = {
 }
 
 export function StoreItem({id, name, price, imgURL}: StoreItemProps){
+  const cartQuantity = 0;
+
   return (
-  <Card>
+  <Card className="h-100">
     <Card.Img variant="top" src={imgURL} height="200px" style={{objectFit: "cover"}}/>
     <Card.Body className="d-flex flex-column">
       <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
         <span className="fs-2">{name}</span>
-        <span className="ms-2 text-muted">{price}</span>
+        <span className="ms-2 text-muted">{formatCurrency(price)}</span>
       </Card.Title>
+      
+      <div className="mt-auto">
+        {cartQuantity === 0 
+        // 1st column
+        ? (<Button className="w-100">+ Add to Cart</Button>) 
+        // 2nd column
+        :  (
+          <div
+            className="d-flex align-items-center flex-column"
+            style={{ gap: ".5rem" }}
+          >
+          <div
+              className="d-flex align-items-center justify-content-center"
+              style={{ gap: ".5rem" }}
+          >
+            <Button>-</Button>
+            <div>
+              <span className="fs-3">{cartQuantity}</span> in cart
+            </div>
+            {/* 3rd column */}
+            <Button>+</Button>
+            {/* 3rd column */}
+          </div>
+          <Button variant="danger" size="sm">Remove</Button>
+          </div>
+          )}
+
+      </div>
     </Card.Body>
   </Card>
   )
