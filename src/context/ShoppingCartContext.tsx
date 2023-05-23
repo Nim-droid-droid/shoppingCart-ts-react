85c65b402d32ebeff49da8eeb36139bb95177222
Nim-droid-droid <levkoz2222@outlook.com>
2023-05-23 16:16:44 +0100

    add type for ShoppingCartContext Context

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,6 +1,17 @@
 import { createContext, useContext, ReactNode } from "react";
 
+
 // initially ShoppingCartContext context val is an empty obj
+type ShoppingCartContext = {
+  // this type needs the ability/funct to add/remove items increment/decrement its quantity
+  // returns a number
+  getItemQuantity: (id: number) => number
+  // doesnt return anything
+  removeItemQuantity: (id: number) => void
+  // no addToCart func cuz its the same thing as incrementCartQuantity
+  incrementCartQuantity: (id: number) => void
+  decrementCartQuantity: (id: number) => void
+}
 const ShoppingCartContext = createContext({});
 
 export function useShoppingCart(){
