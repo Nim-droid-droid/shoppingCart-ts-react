6209ac0a4e77832c3aaa956895b578f729114b8f
Nim-droid-droid <levkoz2222@outlook.com>
2023-06-06 19:18:12 +0100

    Style Cart items/content within shopping cart panel

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,4 +1,4 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 
 // without this get the ERROR: Parameter 'isOpen' implicitly has an 'any' type
@@ -10,7 +10,7 @@ type ShoppingCartProps = {
   // in ShoppingCartContext file isOpen state was passed into ShoppingCart cmpnt as a prop also called isOpen
 export function ShoppingCart({isOpen} : ShoppingCartProps){
 
-  const {closeCart} = useShoppingCart()
+  const { closeCart } = useShoppingCart()
   return(
     // Bootstrap's offvanva gives the panel a slide in effect
       // If you see clicks outside of panel, it closes
@@ -20,6 +20,26 @@ export function ShoppingCart({isOpen} : ShoppingCartProps){
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
+
+      {/* Style cart items using react bootstrap*/}
+      <Offcanvas.Body>
+        {/* 
+        Stack all items on top of each other (by default Stack is vertical).
+        Gap space of 3 to give each item some space between each other.
+        */}
+        <Stack gap={3}>
+
+          {/* Total price styling */}
+          {/* 
+          Margin start auto pushes the total as far to the right as possible
+          Bold font weight
+          font size of five
+           */}
+          <div className="ms-auto fw-bold fs-5">
+          </div>
+        </Stack>
+      </Offcanvas.Body>
+
     </Offcanvas>
   )
 }
