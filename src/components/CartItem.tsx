49c4e6b4bde766584919613a6fa71ed2ba28817e
Nim-droid-droid <levkoz2222@outlook.com>
2023-06-09 22:20:06 +0100

    Created Type for CartItem

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,10 @@
-export function CartItem(){
+type CartItemProps = {
+  id: number
+  quantity: number
+}
+// now id & quantity paras have the type CartItemProps where both id & quantity have number data type
+  // i have destructure id & quantity from CartItemProps
+export function CartItem({ id, quantity }: CartItemProps){
 
   return(
     <>
