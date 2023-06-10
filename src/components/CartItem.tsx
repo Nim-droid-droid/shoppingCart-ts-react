6ad53d663f1aa71cd5fe2fdf170e7c260fd58499
Nim-droid-droid <levkoz2222@outlook.com>
2023-06-10 23:20:20 +0100

    CartItem component finished

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,3 +1,8 @@
+import { Button, Stack } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import storeItems from "../data/items.json"
+import { formatCurrency } from "../utilities/formatCurrency"
+
 type CartItemProps = {
   id: number
   quantity: number
@@ -5,9 +10,41 @@ type CartItemProps = {
 // now id & quantity paras have the type CartItemProps where both id & quantity have number data type
   // i have destructure id & quantity from CartItemProps
 export function CartItem({ id, quantity }: CartItemProps){
+  const { removeItemQuantity } = useShoppingCart()
+  const item = storeItems.find(i => i.id === id)
+
+  if (item == null) return null
 
   return(
-    <>
-    </>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div> {formatCurrency(item.price * quantity)}</div>
+
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeItemQuantity(item.id)}
+      >
+        &times;
+      </Button>
+
+    </Stack>
   )
 }
