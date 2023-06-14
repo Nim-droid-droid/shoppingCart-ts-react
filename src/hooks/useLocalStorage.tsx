6603e607d273fd38ba99b019d7774e1890f694ba
Nim-droid-droid <levkoz2222@outlook.com>
2023-06-15 00:21:51 +0100

    Set up custom hook useLocalStorage

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,12 @@
+// Custom Hook
+// Even if user refreshes the page, items stay in the cart
+
+// make this custom hook work with custom generic types & different props
+
+
+export function useLocalStorage(){
+  return(
+    <>
+    </>
+  )
+}
