21947e59449305afe202c5cf4ba190f90eb32b00
Nim-droid-droid <levkoz2222@outlook.com>
2023-06-16 22:46:46 +0100

    Set up state for generic type T

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,10 +1,21 @@
 // Custom Hook
 // Even if user refreshes the page, items stay in the cart
 
-// make this custom hook work with custom generic types & different props
+import { useState } from "react"
+
+// make this custom hook work with custom generic types (in this case <T>) & different props
 
 
-export function useLocalStorage(){
+
+// <T> has a key which is of str data type, the initial val of the key will be whatever is the type of T or a funct that retruns this type of T
+  // What is the type of T? Whatever i pass (i passed an arr of cart items) into useLocalStorage <> ShoppingCartContext.tsx
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)){
+  // use state logic
+  // useState is going to be of this generic type T
+  const [value, setValue] = useState<T>( () => {
+    
+  })
+
   return(
     <>
     </>
