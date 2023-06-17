52151387aef19d10d7e68fd88c622ebb8415e30d
Nim-droid-droid <levkoz2222@outlook.com>
2023-06-17 20:33:30 +0100

    Created logic to get the val from local storage or get the initial val passed in

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -6,18 +6,21 @@ import { useState } from "react"
 // make this custom hook work with custom generic types (in this case <T>) & different props
 
 
-
 // <T> has a key which is of str data type, the initial val of the key will be whatever is the type of T or a funct that retruns this type of T
   // What is the type of T? Whatever i pass (i passed an arr of cart items) into useLocalStorage <> ShoppingCartContext.tsx
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)){
+
+// Get the val from local storage or get the initial val i passed in
   // use state logic
   // useState is going to be of this generic type T
   const [value, setValue] = useState<T>( () => {
-    
-  })
+    const jsonValue = localStorage.getItem(key)
+    if (jsonValue != null) return JSON.parse(jsonValue)
 
-  return(
-    <>
-    </>
-  )
+    if (typeof initialValue === "function"){
+      return initialValue()
+    } else {
+      return initialValue
+    }
+  })
 }
