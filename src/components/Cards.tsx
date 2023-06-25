1c9c08db9d7738422e1aec7067e988dffd6a7f3c
Nim-droid-droid <levkoz2222@outlook.com>
2023-06-25 01:07:31 +0100

    Added comments

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,3 +1,8 @@
+// Create a card per character
+  // in each card have a link 'browse all toys' that takes them to a page that list out all that chars toys
+
+// apply all ur react knowldg in this prj
+
 export function Cards(){
   return(
     <></>
